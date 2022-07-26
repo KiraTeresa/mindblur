b13db2d7a2c539f57001ab61f1be018e7afe9a6e
André de Albuquerque <itstheandre@gmail.com>
2022-07-26 19:44:48 +0200

    fix: add PORT variable to be able to have dynamic ports on heroku

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
