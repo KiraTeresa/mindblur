72d34cace85b2b7fd6b2163ca606c7efce00fa62
André de Albuquerque <itstheandre@gmail.com>
2022-07-26 19:56:50 +0200

    feat: add logger

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const logger = require("morgan");
 
 function makesConfig(app) {
   app.set("view engine", "hbs");
   app.use(express.urlencoded({ extended: false }));
+  app.use(logger("dev"));
 }
 
 module.exports = makesConfig;
