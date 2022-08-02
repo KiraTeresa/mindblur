316cd7794c76a668f43616b76eac38aada454195
Filipe <filipefreire@protonmail.com>
2022-08-02 21:47:16 +0200

    add: styling

--- config/index.js
+++ config/index.js
@@ -1,27 +1,30 @@
-const express = require("express");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 
-const MongoStore = require("connect-mongo"); // connecting to mongodb to create data
+const MongoStore = require('connect-mongo'); // connecting to mongodb to create data
 
-const session = require("express-session"); // middleware that gives us possibility to deal with sessions
+const session = require('express-session'); // middleware that gives us possibility to deal with sessions
+const path = require('path');
 
 function makesConfig(app) {
-  app.set("view engine", "hbs");
+  app.set('view engine', 'hbs');
   app.use(express.urlencoded({ extended: false }));
-  app.use(logger("dev"));
+  app.use(logger('dev'));
   app.use(cookieParser());
   app.use(
     session({
-      name: "hello-class",
-      secret: process.env.SESSION_SECRET || "lasdkfjghsdfklgjhsdfkljghsdfljkgh",
+      name: 'hello-class',
+      secret: process.env.SESSION_SECRET || 'lasdkfjghsdfklgjhsdfkljghsdfljkgh',
       resave: false,
       saveUninitialized: false,
       store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URL || "mongodb://localhost/mindblur",
+        mongoUrl: process.env.MONGO_URL || 'mongodb://localhost/mindblur',
       }),
     })
   );
+  console.log(path.join(__dirname));
+  app.use(express.static(path.join(__dirname, '..', 'public')));
 
   app.use((req, res, next) => {
     if (req.session.userId) {
